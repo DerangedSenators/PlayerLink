02b701454b83b1fa02349e196985426de4692152
Hanzalah Ravat <hanzalah.ravat@gmail.com>
2020-11-24 15:07:11 +0000

    Add Favicon and Homepage and Copyright

---
+++ doxygen/index.md
@@ -0,0 +1,3 @@
+# PlayerLink
+
+PlayerLink is a cross-platform native multiplayer API with a focus on client-side processing.
--- doxygentomd.sh
+++ doxygentomd.sh
@@ -7,5 +7,6 @@ cp doxygen/mkdocs.yml docs/mkdocs.yml
 ./doxygen/doxybook2 --input xml/ --output docs/ --config doxygen/doxybookcfg.json
 rm -r docs/Files
 rm -r docs/Pages
-
+cp doxygen/favicon.ico docs/img
+cp doxygen/index.md docs/index.md
 echo "Done"
