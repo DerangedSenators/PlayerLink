50cab9402d324646d2f42286b9a72ee02e1961ad
Hanzalah Ravat <hanzalah.ravat@gmail.com>
2020-11-24 14:48:19 +0000

    Improve documentation pages

--- doxygentomd.sh
+++ doxygentomd.sh
@@ -5,5 +5,7 @@ echo "Converting Doxygen XML to Markdown"
 mkdir docs
 cp doxygen/mkdocs.yml docs/mkdocs.yml
 ./doxygen/doxybook2 --input xml/ --output docs/ --config doxygen/doxybookcfg.json
+rm -r docs/Files
+rm -r docs/Pages
 
 echo "Done"
