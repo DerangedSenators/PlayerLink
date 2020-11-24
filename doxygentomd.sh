2cef51d31f33244c8b786847adb5218999c34446
Hanzalah Ravat <hanzalah.ravat@gmail.com>
2020-11-24 11:26:36 +0000

    Add script to convert xml to md

---
+++ doxygentomd.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Converting Doxygen XML to Markdown"
+
+mkdir docs
+cp doxygen/mkdocs.yml docs/mkdocs.yml
+./doxygen/doxybook2 --input xml/ --output docs/ --config doxygen/doxybookcfg.json
+
+echo "Done"
