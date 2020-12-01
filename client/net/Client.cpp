20477a05f8b9fe2e3d0d4291a5437b8ce9989c11
Hanzalah Ravat <hanzalah.ravat@gmail.com>
2020-12-01 14:59:18 +0000

    Initial Client Implementation
    NOTE: Not all methods have been tested and I think there may still be underlying issues with the sendBytes() method and related function calls

--- client/net/Client.cpp
+++ client/net/Client.cpp
@@ -14,7 +14,79 @@
  * limitations under the License.
  */
 #include "Client.h"
+#include <net\ServerCodes.h>
 
 namespace PlayerLink{namespace Client{
 
+	Client::Client(std::string server, std::string port) : mRunSignal(true) {
+		mServer = server;
+		mPort = port;
+		// Try to connnect to the server
+		mClientSocket.connectWithTimeout(server.c_str(), port.c_str(), TIMEOUT_SECONDS, TIMEOUT_MICROSECONDS);
+	}
+
+	Client::~Client() {
+		if (mRunSignal) {
+			mExitMutex.lock();
+			mClientSocket.sendBytes((unsigned char *) DISCONNECT_REQUEST_CODE);
+			mClientSocket.close();
+			mRunSignal = false;
+			mExitMutex.unlock();
+		}
+	}
+
+	bool Client::login(std::string usrName) {
+		mUserName = usrName;
+		unsigned char* userNameAsChar;
+		mClientSocket.sendBytes((unsigned char *)usrName.c_str());
+		mThreadGroup.create_thread(boost::bind(&Client::readLoop,this));
+		mThreadGroup.create_thread(boost::bind(&Client::writeLoop,this));
+
+		mThreadGroup.join_all();
+		mClientSocket.close();
+		return 0;
+	}
+
+	void Client::logout() {
+		mClientSocket.sendBytes((unsigned char*) DISCONNECT_REQUEST_CODE);
+		mExitMutex.lock();
+		mRunSignal = false;
+		mExitMutex.unlock();
+	}
+
+	void Client::addMsgToBuffer(unsigned char* message) {
+		mMessageOutQueue.push_back(message);
+	}
+
+	void Client::readLoop() {
+		unsigned char* bytes;
+		while (mRunSignal) {
+			if (mClientSocket.canReceive(SLEEP_DELAY)) {
+				bytes = mClientSocket.recieveBytes();
+				mMessageInQueue.push_back(bytes);
+				std::this_thread::sleep_for(std::chrono::microseconds(SLEEP_DELAY));
+			}
+		}
+	}
+
+	void Client::writeLoop() {
+		unsigned char* outMsg;
+		char outBuff[DEFAULT_BUFFER_SIZE] = { 0 };
+		while (mRunSignal) {
+			// WIP: Get the buffer and write it to the outmsg
+			if (!mMessageOutQueue.empty()) {
+				//Check if the buffer holds the exit message. If it is, run the logout method...
+				outMsg = mMessageOutQueue.front();
+				if (outMsg[0] == DISCONNECT_REQUEST_CODE) {
+					logout();
+				}
+				else {
+					mMessageOutQueue.pop_front();
+					mClientSocket.sendBytes(outMsg);
+				}
+				outMsg = NULL;
+				memset(outBuff, 0, DEFAULT_BUFFER_SIZE);
+			}
+		}
+	}
 }}
